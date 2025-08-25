3c928b54dba4f9358e7332deb8315d5391e5107d
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-08-25 17:40:21 -0400

    RED: test para cacho, funcionalidades de perder dado y ver dados - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -6,12 +6,28 @@ class TestCacho:
     def test_crear(self):
         cacho = Cacho()
         assert cacho is not None
-        assert cacho.cant_dados() == 5
+        assert cacho.ver_cant_dados() == 5
+
+    def test_visibles_none(self):
+        cacho = Cacho()
+        estado_previo = cacho.visibles
+        cacho.cambiar_visibles()
+        assert cacho.visibles != estado_previo
 
     def test_ver_dados_null(self):
         cacho = Cacho()
         assert cacho.ver_dados() == None
 
+    def test_ver_dados_not_null(self):
+        cacho = Cacho()
+        cacho.cambiar_visibles()
+        cacho.ver_dados()
+
     def test_agitar(self):
         cacho = Cacho()
         cacho.agitar()
+
+    def test_perder_dado(self):
+        cacho = Cacho()
+        cacho.perder_dado()
+        assert cacho.ver_dados() == 4
