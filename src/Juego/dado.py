94ffbffbb5fd3c40a6dd986cdf26171964840fba
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-08-27 00:19:40 -0400

    GREEN: implementacion clase dado - test positivo como esperado

---
+++ src/Juego/dado.py
@@ -0,0 +1,17 @@
+from src.servicios.generador_aleatorio import GeneradorAleatorio
+
+class Dado:
+
+    valores = { 1 : "as", 2 : "tontos",
+                3 : "trenes", 4 : "cuadras",
+                5 : "quinas", 6 : "sextas" }
+
+    valor : int
+
+    def __init__(self):
+        generador = GeneradorAleatorio()
+        self.valor = generador.generar(1,6)
+
+    def obtener_valor(self):
+        return self.valores[self.valor]
+
