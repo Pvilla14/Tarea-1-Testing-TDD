933b65a0036ca7315907332018fa9104b388bcda
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-08-28 15:57:07 -0400

    RED: test para cambiar valor de un dado desde la clase dado - falla como esperado

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -18,3 +18,15 @@ class TestDado(unittest.TestCase):
         assert dado.obtener_valor() == "cuadras"
 
         mock.generar.assert_called_once_with(1,6)
+
+    @patch("src.Juego.dado.GeneradorAleatorio")
+    def test_cambiar_valor(self, GeneradorMock):
+        dado = Dado()
+
+        mock = GeneradorMock.return_value
+        mock.generar.return_value = 5
+
+        dado.cambiar_valor()
+
+        assert dado.obtener_valor() == "quinas"
+
