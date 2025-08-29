4eedf5d500564911a0094e5c137399e951353d5d
daaburto <daaburto2023@inf.udec.cl>
2025-08-29 01:26:37 -0400

    RED: test para resolver calces - falla como esperado

--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -14,3 +14,17 @@ def test_resolver_duda_pierde_dudador():
     conteo_real = 3
     resultado = arbitro.resolver_duda(apuesta, conteo_real)
     assert resultado == 'Dudador pierde'
+
+def test_resolver_calce_gana_apostador():
+    arbitro = ArbitroRonda()
+    apuesta = (2, 5)
+    conteo_real = 2
+    resultado = arbitro.resolver_calce(apuesta, conteo_real)
+    assert resultado == 'Apostador gana'
+
+def test_resolver_calce_pierde_apostador():
+    arbitro = ArbitroRonda()
+    apuesta = (2, 5)
+    conteo_real = 3
+    resultado = arbitro.resolver_calce(apuesta, conteo_real)
+    assert resultado == 'Apostador pierde'
