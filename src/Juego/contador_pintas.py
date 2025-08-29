e58b1419a32157bb1f1c184422e064ca9afc7b0f
daaburto <daaburto2023@inf.udec.cl>
2025-08-28 23:42:05 -0400

    GREEN: implementación mínima para contar pintas sin comodines

---
+++ src/Juego/contador_pintas.py
@@ -0,0 +1,27 @@
+class ContadorPintas:
+    """
+    Cuenta apariciones de pintas en múltiples cachos.
+    """
+
+    def contar_pinta(self, pinta, cachos, usar_comodines=True):
+        """
+        Cuenta cuántas veces aparece una pinta específica en todos los cachos.
+
+        Args:
+            pinta (str): Pinta a contar
+            cachos (list): Lista de objetos Cacho
+            usar_comodines (bool): Si los ases cuentan como comodines
+
+        Returns:
+            int: Número total de dados con esa pinta
+        """
+        contador = 0
+
+        for cacho in cachos:
+            for dado in cacho.dados:
+                valor_dado = dado.obtener_valor()
+
+                if valor_dado == pinta:
+                    contador += 1
+
+        return contador
