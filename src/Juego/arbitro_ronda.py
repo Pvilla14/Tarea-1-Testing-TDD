6a567bc18d086309dd6e40c5adf3d35eb600e48c
daaburto <daaburto2023@inf.udec.cl>
2025-08-29 16:57:14 -0400

    GREEN: implementación mínima para validar calces

--- src/Juego/arbitro_ronda.py
+++ src/Juego/arbitro_ronda.py
@@ -12,3 +12,16 @@ class ArbitroRonda:
             return 'Apostador gana'
         else:
             return 'Apostador pierde'
+
+    def validar_calce(self, dados_restantes, dados_jugador, dados_iniciales):
+        if dados_jugador == 1:
+            return True
+
+        mitad_dados = self.mitad(dados_iniciales)
+        if dados_restantes >= mitad_dados:
+            return True
+
+        return False
+
+    def mitad(self, total):
+        return (total + 1) // 2
