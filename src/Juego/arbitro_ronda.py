ec22e1b23d1a3c330774511bd61b550c0dde5f6e
daaburto <daaburto2023@inf.udec.cl>
2025-08-29 01:27:15 -0400

    GREEN: implementación mínima para resolver calces - falla como esperado

--- src/Juego/arbitro_ronda.py
+++ src/Juego/arbitro_ronda.py
@@ -5,3 +5,10 @@ class ArbitroRonda:
             return 'Apostador pierde'
         else:
             return 'Dudador pierde'
+
+    def resolver_calce(self, apuesta, conteo_real):
+        cantidad, pinta = apuesta
+        if conteo_real == cantidad:
+            return 'Apostador gana'
+        else:
+            return 'Apostador pierde'
