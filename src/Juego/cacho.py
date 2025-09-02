777e6914652690dfbca13e7d8ab2e15379cc4847
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-09-02 11:20:39 -0400

    GREEN: se implemento la recuperacion de un dado y los dados extra

--- src/Juego/cacho.py
+++ src/Juego/cacho.py
@@ -5,13 +5,18 @@ class Cacho:
     cant_dados : int
     visibles : bool = False
     dados : list[Dado]
+    dados_extra : int
 
     def __init__(self):
         self.cant_dados = 5
         self.dados = [Dado() for _ in range(self.cant_dados)]
+        self.dados_extra = 0
 
     def ver_cant_dados(self):
         return self.cant_dados
+
+    def ver_dados_extra(self):
+        return self.dados_extra
 
     def cambiar_visibles(self):
         self.visibles = not self.visibles
@@ -23,8 +28,21 @@ class Cacho:
             return [dado.obtener_valor() for dado in self.dados]
 
     def perder_dado(self):
-        self.cant_dados -= 1
+        if self.dados_extra == 0:
+            self.cant_dados -= 1
+            self.dados.pop()
+
+        else:
+            self.dados_extra -= 1
 
     def agitar(self):
         for dado in self.dados:
             dado.cambiar_valor()
+
+    def recuperar_dado(self):
+        if self.cant_dados == 5:
+            self.dados_extra += 1
+
+        else:
+            self.dados.append(Dado())
+            self.cant_dados += 1
