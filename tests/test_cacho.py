ea159ec0a89d2095235a773f80b8edf6b27b6798
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-09-02 11:19:59 -0400

    RED: test para cacho, probando la funcionalidad de recuperar un dado - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -1,7 +1,7 @@
-import pytest
+import unittest
 from src.Juego.cacho import Cacho
 
-class TestCacho:
+class TestCacho(unittest.TestCase):
 
     def test_crear(self):
         cacho = Cacho()
@@ -16,21 +16,44 @@ class TestCacho:
 
     def test_ver_dados_null(self):
         cacho = Cacho()
-        assert cacho.ver_dados() == None
+        self.assertTrue(cacho is not None)
 
     def test_ver_dados_not_null(self):
         cacho = Cacho()
         cacho.cambiar_visibles()
-        assert cacho.ver_dados() is not None
+        self.assertTrue(cacho.ver_dados() is not None)
 
     def test_agitar(self):
         cacho = Cacho()
         cacho.cambiar_visibles()
         dados = cacho.ver_dados()
         cacho.agitar()
-        assert dados != cacho.ver_dados()
+        self.assertNotEqual(dados,cacho.ver_dados())
 
     def test_perder_dado(self):
         cacho = Cacho()
         cacho.perder_dado()
-        assert cacho.ver_cant_dados() == 4
+        self.assertEqual(cacho.ver_cant_dados(), 4)
+
+    def test_recuperar_dado(self):
+        cacho = Cacho()
+        cacho.perder_dado()
+        self.assertEqual(cacho.ver_cant_dados(), 4)
+
+        cacho.recuperar_dado()
+        self.assertEqual(cacho.ver_cant_dados(), 5)
+
+    def test_dado_extra(self):
+        cacho = Cacho()
+        self.assertEqual(cacho.ver_dados_extra(), 0)
+        cacho.recuperar_dado()
+        self.assertEqual(cacho.ver_cant_dados(), 5)
+        self.assertEqual(cacho.ver_dados_extra(), 1)
+
+    def test_perder_dado_con_dado_extra(self):
+        cacho = Cacho()
+        cacho.recuperar_dado()
+        self.assertEqual(cacho.ver_dados_extra(), 1)
+        cacho.perder_dado()
+        self.assertEqual(cacho.ver_cant_dados(), 5)
+        self.assertEqual(cacho.ver_dados_extra(), 0)
